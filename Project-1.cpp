eb353d2dd8d12cafefc757e4e1e60fd17ee6f6d2
Ghostnooob123 <hero.zerodoku@gmail.com>
2022-12-14 22:57:24 +0200

    Project-1 is finished 100%

--- Project-1.cpp
+++ Project-1.cpp
@@ -6,7 +6,7 @@
 int main()
 {
 
-	std::cout << "Enter a integers lenght: ";
+	std::cout << "Enter an integers to be sorted: ";
 	int length{};
 	std::cin >> length;
 
@@ -18,26 +18,32 @@ int main()
 		std::cin >> integers[i];
 	}
 
-	std::cout << "How you want your array to be sorted by descending or ascending.";
+	std::cout << '\n';
+	std::cout << "How you want your array to be sorted by ascending or descending.\n";
+	std::cout << "Enter 1 for ascending / Enter 2 for descending: ";
+	std::cout << '\n';
 	int way{};
-	std::cin >> way;
+	std::cin >> way; 
 
 	if (way == 1)
 	{
+		std::cout << "Here is your sorted list by ascending: \n";
 		std::sort(integers, integers + length);
 	}
 	else if (way == 2)
 	{
+		std::cout << "Here is your sorted list by descending: \n";
 		std::sort(integers, integers + length ,std::greater<int>());
 	}
 	else
-	{
-		std::cout << "Try again later :) ";
-	}
+		std::cout << "Try again later :) \n";
 
 	for (int i = 0; i < length; i++)
 	{
 		std::cout << "Integer #" << i + 1 << ": " << integers[i] << '\n';
 	}
+
+	delete[] integers;
+	return 0;
 }
 
